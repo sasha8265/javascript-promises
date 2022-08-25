008cc26f5a47ea4801286fbadcb1c531686d59bc
Sasha Czerniawski <sashacandoit@gmail.com>
2022-08-24 20:15:54 -0400

    talking to api and getting facts about random numbers

--- promises_part_1.js
+++ promises_part_1.js
@@ -2,16 +2,26 @@ const baseURL = "http://numbersapi.com/"
 const randomTrivia = "random/trivia"
 
 
-let fourNumbers = [];
-
-for (let i = 1; i < 5; i++) {
-    fourPokemonPromises.push(
-        axios.get(`https://pokeapi.co/api/v2/pokemon/${i}/`)
-    );
+let fiveRandomNumbers = []
+for (let i = 0; i < 5; i++) {
+    fiveRandomNumbers.push(Math.round(Math.random() * 500))
 }
+console.log(fiveRandomNumbers)
 
-Promise.all(fourPokemonPromises)
-    .then(pokemonArr => (
-        pokemonArr.forEach(p => console.log(p.name))
-    ))
-    .catch(err => console.log(err));
+
+let fiveRandomFacts = []
+for (let i = 0; i < fiveRandomNumbers.length; i++) {
+        fiveRandomFacts.push(
+            axios.get(`http://numbersapi.com/${fiveRandomNumbers[i]}`)
+        );
+    }
+
+console.log(fiveRandomFacts)
+
+
+
+// Promise.all(fourPokemonPromises)
+//     .then(pokemonArr => (
+//         pokemonArr.forEach(p => console.log(p.name))
+//     ))
+//     .catch(err => console.log(err));
