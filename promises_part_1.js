215eeb089b8beea4f55aa40b9962c90c7819ce59
Sasha Czerniawski <sashacandoit@gmail.com>
2022-08-27 10:55:10 -0400

    printing favorite number data done

--- promises_part_1.js
+++ promises_part_1.js
@@ -1,24 +1,48 @@
-const baseURL = "http://numbersapi.com/"
-const randomTrivia = "random/trivia"
+let myNumber = 12;
+let fiveNumbers = [8, 60, 31, 15, 72]
+const baseURL = "http://numbersapi.com/";
+// const randomTrivia = "random/trivia"
 
 
-let fiveNumbers = [1,2,3,4,5]
-
-let fiveRandomFacts = []
-for (let i = 0; i < fiveNumbers.length; i++) {
-    fiveRandomFacts.push(
-        axios.get(`http://numbersapi.com/random/trivia?json`)
-    );
-}
+//1
+$.getJSON(`${baseURL}${myNumber}?json`)
+    .then(data => {
+        console.log(data)
+        console.log(data.text)
+    })
 
 
-Promise.all(fiveRandomFacts)
-    .then(fiveRandomFacts => (
-        fiveRandomFacts.forEach(numFact => {
-            console.log(numFact.data)
-        })
-    ))
-    .catch(err => console.log(err));
+//2
+$.getJSON(`${baseURL}${fiveNumbers}?json`)
+    .then(data => {
+        console.log(data)
+        
+    })
+
+
+
+// let randomFactData = []
+// for (let i = 0; i < fiveNumbers.length; i++) {
+//     randomFactData.push(
+//         axios.get(`http://numbersapi.com/random/trivia?json`)
+//     );
+// }
+
+// fiveRandomFacts = []
+
+// Promise.all(randomFactData)
+//     .then(randomFactData => (
+//         randomFactData.forEach(numFact => {
+//             let factData = [
+//                 numFact.data.number,
+//                 numFact.data.text
+//             ]
+//             fiveRandomFacts.push(factData)
+//         })
+//     ))
+//     .catch(err => console.log(err));
+    
+// console.log(fiveRandomFacts)
 
 
 
