5cacd8695a72e35913da5020f4ef284466abaf5b
Sasha Czerniawski <sashacandoit@gmail.com>
2022-08-27 12:23:55 -0400

    question 3 finished

--- promises_part_1.js
+++ promises_part_1.js
@@ -1,15 +1,14 @@
 let myNumber = 12;
 let fiveNumbers = [8, 60, 31, 15, 72]
 const baseURL = "http://numbersapi.com/";
-// const randomTrivia = "random/trivia"
 
 
 //1
 $.getJSON(`${baseURL}${myNumber}?json`)
     .then(data => {
-        console.log(data)
-        console.log(data.text)
-        console.log(myNumber)
+        // console.log(data)
+        // console.log(data.text)
+        // console.log(myNumber)
         $("#question-1").append(`<h2 class="card-title text-primary">#${myNumber}</h2>`)
         $("#question-1").append(`<p class="card-text text-secondary">${data.text}</p>`)
     })
@@ -19,7 +18,7 @@ $.getJSON(`${baseURL}${myNumber}?json`)
 for (num of fiveNumbers) {
     $.getJSON(`${baseURL}${num}?json`)
         .then(data => {
-            console.log(data)
+            // console.log(data)
             $("#question-2").append(
                 `<div class="my-4 card" id="card-${data.number}">
                     <div class="card-body">
@@ -31,36 +30,14 @@ for (num of fiveNumbers) {
 }
 
 
+//3
+Promise.all(
+    Array.from({ length: 4 }, () => {
+        return $.getJSON(`${baseURL}${myNumber}?json`)
+    })
+)
+    .then(facts => {
+        facts.forEach(data => $("#q-3-list").append(`<li class="card-text text-secondary mt-3">${data.text}</li>`));
+    });
 
 
-
-// $.getJSON(`${baseURL}${fiveNumbers}?json`)
-    
-    // .then(facts => {
-    //     facts.forEach(data => $("#card-1").append(`<h2>${data.number}</h2>`));
-    // });
-
-
-
-// let randomFactData = []
-// for (let i = 0; i < fiveNumbers.length; i++) {
-//     randomFactData.push(
-//         axios.get(`http://numbersapi.com/random/trivia?json`)
-//     );
-// }
-
-// fiveRandomFacts = []
-
-// Promise.all(randomFactData)
-//     .then(randomFactData => (
-//         randomFactData.forEach(numFact => {
-//             let factData = [
-//                 numFact.data.number,
-//                 numFact.data.text
-//             ]
-//             fiveRandomFacts.push(factData)
-//         })
-//     ))
-//     .catch(err => console.log(err));
-    
-// console.log(fiveRandomFacts)
